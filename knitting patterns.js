88d9e6b17fadbab3024eae3197eb37efb9ac3652
PasR-Hi9 <92518599+PasR-Hi9@users.noreply.github.com>
2022-02-01 13:54:30 +0000

    added annotations

--- knitting patterns.js
+++ knitting patterns.js
@@ -1,23 +1,23 @@
-function getGage(){
-  const widthGage = window.prompt('How many stitches across is 10cm in your gage?')
-  const heightGage = window.prompt('How many stitches high is 10cm in your gage?')
+function getGage(){ //this function finds their stitches per cm in both vertical and horizontal planes
+  const widthGage = 0.1*(window.prompt('How many stitches across is 10cm in your gage?'))
+  const heightGage = 0.1*(window.prompt('How many stitches high is 10cm in your gage?'))
 }
 
-function makeScarf(){
+function makeScarf(){ //this simplest pattern there is - just divides normal hat proportions by gage
   getGage()
-  let stitchesAcross = Math.ceil(widthGage*1.524)
-  let rowNumber = Math.ceil(heightGage*16.764)
+  let stitchesAcross = Math.ceil(widthGage*15.24)
+  let rowNumber = Math.ceil(heightGage*167.64)
   console.log(`Cast on ${stitchesAcross} stitches. Knit every row for ${rowNumber} rows. Cast off.`)
 }
 
 function makeHat(){
   getGage()
   let widthNeeded = window.prompt("What is the circumference of the head you're making a hat for in cm?")
-  let heightNeeded = widthNeeded/2.5
-  let rowNumber = Math.ceil(heightGage*0.1*heightNeeded)
-  let bandWidth = Math.ceil(widthNeeded/10)
+  let heightNeeded = widthNeeded/2.5 //calculates based on average head width to height ratio
+  let rowNumber = Math.ceil(heightGage*heightNeeded) //gives number of rows
+  let bandWidth = Math.ceil(widthNeeded/10) //takes height of ribbing as a proportion of head size
   let hatHeight = Math.ceil(heightNeeded - bandWidth)
-  let stitchesAcross = Math.ceil(widthGage*0.1*widthNeeded)
+  let stitchesAcross = Math.ceil(widthGage*widthNeeded)
   if (widthNeeded%2 == 0) {widthNeeded = widthNeeded + 1}
   let stitchesOff = Math.floor(stitchesAcross/4)
   console.log(`Cast on ${stitchesAcross} in your preferred method. Knit in 1 by 1 ribbing with smaller needles for ${bandWidth}cm. Then change to needles one size up and knit in stockinette for ${hatHeight}cm. From then, shape top of hat by alternating rows of K1, K2tog and knitting in stockinette. Once you only have ${stitchesOff} stitches remaining, cast off by pushing a threaded tapestry needle through the remaining stitches and sliding them off the needle. Pull these tight and sew the two sides of the hat together with mattress stitch. Ta da!`)
